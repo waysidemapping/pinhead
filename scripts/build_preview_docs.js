ef6245b03390c869f5368ac51a79a564f537e565
Quincy Morgan <2046746+quincylvania@users.noreply.github.com>
2026-02-21 11:25:53 -0500

    Include migrations.json on the docs site since it might be useful

--- scripts/build_preview_docs.js
+++ scripts/build_preview_docs.js
@@ -5,7 +5,8 @@ const version = JSON.parse(readFileSync('package.json')).version;
 console.log('Building docs for Pinhead v' + version);
 const majorVersion = parseInt(version.split('.')[0]);
 
-copyFileSync('package.json', "docs/package.json")
+copyFileSync('package.json', "docs/package.json");
+copyFileSync('migrations.json', "docs/migrations.json");
 
 ensureEmptyDir(`docs/v${majorVersion}`);
 execSync(`cp -r "dist/icons/" 'docs/v${majorVersion}'`);
