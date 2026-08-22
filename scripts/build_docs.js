0e96c0bd7627ffdb1aa4923be683e2f8a5a0d569
Quincy Morgan <2046746+quincylvania@users.noreply.github.com>
2026-08-22 09:37:28 -0400

    Run all build commands from node file

--- scripts/build_docs.js
+++ scripts/build_docs.js
@@ -1,5 +1,6 @@
 import { execFileSync } from "child_process";
 import { join } from "path";
+import { writeFileSync } from "fs";
 import { downloadExternalSourceAssets } from "../src/ExternalSourceManager.js";
 import { downloadLegacyAssets } from "../src/LegacyAssetManager.js";
 
@@ -10,5 +11,12 @@ const docsDir = "docs/";
 execFileSync("cp", ["-r", `src/.`, join(docsDir, `src`)]);
 downloadExternalSourceAssets(join(docsDir, "srcicons"));
 downloadLegacyAssets(docsDir);
+
+execFileSync("cp", [`metadata/categories.json`, join(docsDir, `categories.json`)]);
+
+const npmPublishDates = execFileSync("npm", ["view", "@waysidemapping/pinhead", "time", "--json"], {
+  encoding: "utf8",
+});
+writeFileSync(join(docsDir, `npm_publish_dates.json`), npmPublishDates);
 
 console.log("Done building docs");
